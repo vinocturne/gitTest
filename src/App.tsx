1b120f321a7c38745a6d011714658807fd4c93a4
shinghunhee <godgh@twave.co.kr>
2025-01-08 08:52:12 +0900

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          master abc
-
-          feature2 test
-          one more test
-
-          plus new
-        </a>
+        delete
       </header>
     </div>
   );
